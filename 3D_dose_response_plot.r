cb1d45ca05db09940a6b055fdcc0b048dfcb41d5
Bryan Queme <bryanmqueme@gmail.com>
2019-07-15 08:46:32 -0400

    Update and rename 3D_dose_response_plot.py to 3D_dose_response_plot.r

---
+++ 3D_dose_response_plot.r
@@ -0,0 +1,8 @@
+########## 3D DOSE RESPONSE PROGRAM ##########
+
+#The purpose of this program is to take a pre-processed .csv file and create 3D plot
+
+
+library(rgl)
+library(RColorBrewer)
+library(data.table)
